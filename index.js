43fb3e79eb58aea5bc54a69c93f0d3b5b3a8e45f
msktgd-lang <mskt.gd@gmail.com>
2026-07-18 00:11:06 +0300

    index.js

--- index.js
+++ index.js
@@ -146,83 +146,67 @@ app.post("/webhook", async(req,res)=>{
  // أمر البداية
  //=====================
 
- if(text === "/start"){
+if (text === "/start") {
 
+  await fetch(API + "/sendMessage", {
 
-  await fetch(API + "/sendMessage",{
+    method: "POST",
 
-   method:"POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
 
-   headers:{
-    "Content-Type":"application/json"
-   },
+    body: JSON.stringify({
 
+      chat_id: chatId,
 
-   body:JSON.stringify({
-
-    chat_id:chatId,
-
-    text:
+      text:
 `🌹 أهلاً بك في بوت MOQATI511
 
 اختر الخدمة المطلوبة:`,
 
+      reply_markup: {
 
-    reply_markup:{
+        inline_keyboard: [
 
-     inline_keyboard:[
+          [
+            {
+              text: "📱 إضافة رقم الجوال",
+              url: "https://script.google.com/macros/s/AKfycbwhcbIigHH5S9_gKfjBAvry92gyps3pR2ZIMKh9knLrAprWR9LG1djRZPZm0Eq-pftZnw/exec"
+            }
+          ],
 
+          [
+            {
+              text: "📅 إضافة موعد الزواج",
+              url: "https://script.google.com/macros/s/AKfycbw6yH_qWiFlZ9lCy5_bjw5CSPf8Cgz_c1aWxJ-s6x10yrDhwrTK7fUPRKYeE_h1oze-/exec"
+            }
+          ],
 
-      [
-       {
-        text:"📱 إضافة رقم الجوال",
-        callback_data:"add_phone"
-       }
-      ],
+          [
+            {
+              text: "📋 جدول زواجات القبيلة",
+              url: "https://script.google.com/macros/s/AKfycbwFdO1vFM08rqugX5FXi-Tyo69vgr2dbL7uS1XiqYg7IsWoBVjMEzA31WQ4q4LRlNXo1w/exec"
+            }
+          ],
 
+          [
+            {
+              text: "☎️ للتواصل معنا",
+              url: "https://api.whatsapp.com/send/?phone=966500994990&text&type=phone_number&app_absent=0"
+            }
+          ]
 
-      [
-       {
-        text:"📅 إضافة موعد الزواج",
-        callback_data:"add_wedding"
-       }
-      ],
+        ]
 
+      }
 
-      [
-       {
-        text:"📋 جدول زواجات القبيلة",
-        url:
-"https://script.google.com/macros/s/AKfycbwFdO1vFM08rqugX5FXi-Tyo69vgr2dbL7uS1XiqYg7IsWoBVjMEzA31WQ4q4LRlNXo1w/exec"
-       }
-      ],
-
-
-      [
-       {
-        text:"☎️ للتواصل معنا",
-        url:
-"https://api.whatsapp.com/send/?phone=966500994990&text&type=phone_number&app_absent=0"
-       }
-      ]
-
-
-     ]
-
-    }
-
-
-   })
-
+    })
 
   });
 
-
   return res.sendStatus(200);
-
- }
-
-
+}
 
  await sendMessage(
   chatId,
